dfc2f2f432fbf91ee0df35bad4762e37962ab709
RMiller28 <Millerrycki5@gmail.com>
2014-03-26 10:14:46 -0400

    Finish

--- GetVowels.cs/Program.cs
+++ GetVowels.cs/Program.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Sum.cs
+namespace GetVowel
 {
     class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                String input;
+
+                Console.WriteLine("Enter a Vowel.");
+                input = Console.ReadLine().ToLower();
+                if (input == "a" || input == "e" || input == "i" || input == "o" || input == "u")
+                {
+                    Console.WriteLine("Now Enter Another Vowel");
+
+                }
+                else if (input != "a" || input != "e" || input != "i" || input != "o" || input != "u")
+                {
+                    break;
+                }
+            }
         }
     }
 }
